839aac683ae46f83345a41ec0a4e9ad4f956eb21
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-11-27 18:50:51 +0300

    Добавит маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainPage from '../../pages/main-page/main-page';
+import FavoritesPage from '../../pages/favourites-page/favourites-page';
+import LoginPage from '../../pages/favourites-page/favourites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import {AppRoute} from '../../consts';
+
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 
 type AppProps = {
   placeCardsCount: number;
@@ -6,7 +13,30 @@ type AppProps = {
 
 function App({placeCardsCount}: AppProps): JSX.Element {
   return (
-    <MainPage placeCardsCount={placeCardsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage placeCardsCount={placeCardsCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,10 @@
 export const Setting = {
   placeCardsCount: 5
 };
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+}
