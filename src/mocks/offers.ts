1d56f72845e64b7e51f5fc031ba6535b1d5b63b1
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-12-03 21:53:38 +0300

    Добавит моки предложений и описание их типов

---
+++ src/mocks/offers.ts
@@ -0,0 +1,98 @@
+import {Offers} from '../types/offers';
+
+export const offers: Offers[] = [
+  {
+    'id': '5167e8fb-6b3c-4731-9b74-50d6ae4f6344',
+    'title': 'Waterfront with extraordinary view',
+    'type': 'room',
+    'price': 262,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/1.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.83961,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4
+  },
+  {
+    'id': '9911109a-783a-4ebd-910e-7ad44c1b58fa',
+    'title': 'Beautiful & luxurious apartment at great location',
+    'type': 'apartment',
+    'price': 177,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/15.jpg',
+    'city': {
+      'name': 'Cologne',
+      'location': {
+        'latitude': 50.938361,
+        'longitude': 6.959974,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 50.957361,
+      'longitude': 6.9509739999999995,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 3.5
+  },
+
+  {
+    'id': '4b50df95-ad80-41d4-adbc-c6727f2abb90',
+    'title': 'Beautiful & luxurious apartment at great location',
+    'type': 'hotel',
+    'price': 138,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/5.jpg',
+    'city': {
+      'name': 'Brussels',
+      'location': {
+        'latitude': 50.846557,
+        'longitude': 4.351697,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 50.852557,
+      'longitude': 4.3376969999999995,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.8
+  },
+
+  {
+    'id': 'f13ffbb4-b7f2-4925-9312-4f64566784fb',
+    'title': 'Penthouse, 4-5 rooms + 5 balconies',
+    'type': 'apartment',
+    'price': 134,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.36354,
+      'longitude': 4.911976,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 2
+  },
+];
---
+++ src/types/offers.ts
@@ -0,0 +1,24 @@
+export type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+export type OfferCity = {
+    name: string;
+    location: Location;
+};
+
+export type Offers = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  city: OfferCity;
+  location: Location;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  previewImage: string;
+  }
+
