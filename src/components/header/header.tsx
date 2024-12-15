0d12d12301e7ca2abd3d9adc5169ab7ed31cd31d
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-12-15 23:04:15 +0300

    вносит правки в соответствии с  замечаниями наставника

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,6 @@
 import Logo from '../../components/logo/logo';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../consts';
 
 function Header(): JSX.Element {
   return (
@@ -11,10 +13,10 @@ function Header(): JSX.Element {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile">
+                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
                   <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                   <span className="header__login">Sign in</span>
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
