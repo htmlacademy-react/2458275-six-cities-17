b21188c3a9dc6dbc47164abb3db945f4b9a14280
KseniiaPan <panovskaia.ksenia74@gmail.com>
2025-01-09 23:15:38 +0300

    Убирает ошибку в URL, используемую для проверки работы toastify

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,7 +14,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 };
 const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
-const BACKEND_URL = 'https://16.design.htmlacademy.pro/six-citie';
+const BACKEND_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
